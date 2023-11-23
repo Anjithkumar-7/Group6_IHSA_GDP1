bec88934b014e83bbcd73dcb897e03bbe67e0079
RajithaChintala <112004154+RajithaChintala@users.noreply.github.com>
2023-11-22 18:13:50 -0600

    Changes to the page render based on the admin role they
    Hiding some of the specific features based on the role

--- public/js/events.js
+++ public/js/events.js
@@ -4,26 +4,42 @@ document.addEventListener('DOMContentLoaded', async function () {
     const urlParams = new URLSearchParams(window.location.search);
     var EventID = urlParams.get('EventID');
     var eventName = urlParams.get('eventname');
-    
-    if(eventName && EventID){
+
+    if (eventName && EventID) {
+    // if (EventID) {
 
         const links = document.querySelectorAll('.admin-btn');
         links.forEach(link => {
-        const href = link.getAttribute('href');
-        link.setAttribute('href', `${href}?eventname=${eventName}&EventID=${EventID}`);
+            const href = link.getAttribute('href');
+            // link.setAttribute('href', `${href}?eventname=${eventName}&EventID=${EventID}`);
+            link.setAttribute('href', `${href}?eventname=${eventName}&EventID=${EventID}`);
+            //Uncomment this to add/change the url in the admin.html page to add hyperlinks to all the cards.
+            // link.setAttribute('href', `${href}?EventID=${EventID}`);
         });
-        document.getElementById("dashboardName").innerText = eventName + " - Dashboard";
-     /*  const result = await fetch('/createEventDashboard', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                eventName : eventName
-            })
-        })
-        .then((res) => res.json());
-        console.log(result);  */
-}
+
+        //Hide the particular card using the anchor id for that particular element.
+        if(sessionStorage.usertype == 'show_admin'){
+            var scheduleCard = document.getElementById("schedule-card");
+            scheduleCard.classList.add("hidden-card-class");
+            var usersCard = document.getElementById("useraccess-card");
+            usersCard.classList.add("hidden-card-class");
+            var eventsHeaderLink = document.getElementById("events-hyper-link-header");
+            eventsHeaderLink.classList.add("hidden-card-class");
+            
+        }
+        // document.getElementById("dashboardName").innerText = eventName + " - Dashboard";
+        document.getElementById("dashboardName").innerText =  eventName + " - Admin Dashboard";
+        /*  const result = await fetch('/createEventDashboard', {
+               method: 'POST',
+               headers: {
+                   'Content-Type': 'application/json',
+               },
+               body: JSON.stringify({
+                   eventName : eventName
+               })
+           })
+           .then((res) => res.json());
+           console.log(result);  */
+    }
 
 });
--- public/js/login.js
+++ public/js/login.js
@@ -2,6 +2,42 @@ let redirecting = false;
 const token = sessionStorage.getItem("token");
 
 
+document.addEventListener("DOMContentLoaded", async function () {
+  const usertypeSelect = document.getElementById("usertype");
+  const eventTypeSelect = document.getElementById("eventType");
+
+  // Add an event listener to the usertype select dropdown
+  usertypeSelect.addEventListener("change", function () {
+    const selectedValue = usertypeSelect.value;
+
+    // Check if the selected value is "admin" and hide eventTypeSelect
+    if (selectedValue === "admin") {
+      eventTypeSelect.style.display = "none";
+    } else {
+      // For any other value, show eventTypeSelect
+      eventTypeSelect.style.display = "block";
+    }
+  });
+
+  // alert('dom laoded');
+  const result = await fetch('/getEvents', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+    .then((res) => res.json())
+  console.log('Result ===>', result);
+
+  let eventsResult = [];
+  // result.events.forEach(function (element, index) {
+  //   eventsResult.push("<option value='" + element.EventID + "'>" + element.Eventname + "</option>");
+  // })
+  result.events.map(element => {
+    eventsResult.push("<option value='" + element.EventID + "'>" + element.EventName + "</option>");
+  })
+  document.getElementById("eventType").innerHTML = eventsResult;
+});
 
 function checkTokenAndRedirect() {
   if (redirecting) {
@@ -25,51 +61,89 @@ window.addEventListener("popstate", () => {
 
 checkTokenAndRedirect();
 window.addEventListener("hashchange", () => {
-    checkTokenAndRedirect();
-  });
+  checkTokenAndRedirect();
+});
 
 
+//Function to change the display property of dropdown starts.
+// let selectBox = document.getElementById('usertype');
+//   let selectedValue = selectBox.value;
 
+//   let element = document.getElementById("eventType");
+//   // alert(selectedValue);
 
+//   if (selectedValue == 'admin') {
+//     element.setAttribute("hidden", "hidden");
+//   }
 
-  
+// function changeDropDown() {
+//   let selectBox = document.getElementById('usertype');
+//   let selectedValue = selectBox.value;
 
-  
+//   let element = document.getElementById("eventType");
+//   alert(selectedValue);
+
+//   if (selectedValue == 'admin') {
+//     element.setAttribute("hidden", "hidden");
+//   }
+//   if(selectedValue == 'show_admin'){
+//     element.removeAttribute("hidden");
+//   }
+// }
+
+///Function to change the dropdown value ends.
+
 const login = document.getElementById('loginForm');
 login.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    const object = {};
-    data.forEach((value, key) => {
-        object[key] = value;
-    });
-    console.log(object)
-    const result = await fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(object),
+  e.preventDefault();
+
+
+  //--To get the value of the event type from the login form itself without getting the data from the api.
+  // var e = document.getElementById("eventType");
+  // var text = e.options[e.selectedIndex].text;
+  // alert(text);
+
+
+  const data = new FormData(e.target);
+  const object = {};
+  data.forEach((value, key) => {
+    object[key] = value;
+  });
+  console.log(object);
+  const result = await fetch('/showAdminLogin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(object),
+  })
+    .then(response => response.json())
+    .then(responseData => {
+      if (responseData.success) {
+        console.log(responseData);
+        let eventIdFromResponse = responseData.user.eventId;
+        let eventnameFromResponse = responseData.user.EventName;
+        sessionStorage.setItem("token", responseData.token);
+        sessionStorage.setItem("userData", responseData.user);
+        sessionStorage.setItem("usertype", object.usertype);
+
+        //window.location.href="adminEvents.html";
+        const token = sessionStorage.getItem("token");
+
+        if (token && object.usertype == 'admin') {
+          window.location.href = "/adminEvents.html";
+          // window.location.href = `/admin.html?eventname=${eventname}&EventID=${EventID}`;
+        }
+        else if (token && object.usertype == 'show_admin') {
+          // window.location.href = `/admin.html?EventID=${eventIdFromResponse}`;
+          window.location.href = `/admin.html?eventname=${eventnameFromResponse}&EventID=${eventIdFromResponse}`;
+        } else {
+          window.location.href = "/adminLogin.html";
+        }
+      } else {
+        console.log("responseData error")
+        window.location.href = "/adminLogin.html";
+      }
     })
-        .then(response => response.json())
-        .then(responseData => {
-            if (responseData.success) {
-                console.log(responseData);
-                sessionStorage.setItem("token", responseData.token);
-                //window.location.href="adminEvents.html";
-                const token = sessionStorage.getItem("token");
-
-                if (token) {
-                    
-                    window.location.href = "/adminEvents.html"; 
-                } else {
-
-                    window.location.href = "/adminLogin.html";
-                }
-            } else {
-                console.log("responseData error")
-                window.location.href = "/adminLogin.html";
-            }
-        })
 });
 
